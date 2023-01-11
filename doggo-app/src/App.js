01a51d5c51f8eea46e8730f7bab040a00d9f3f25
punam102 <jagtappunam129@gmail.com>
2023-01-11 16:35:32 +0530

    formvalidation

--- doggo-app/src/App.js
+++ doggo-app/src/App.js
@@ -1,10 +1,11 @@
-
-import './App.css';
-
+import "./App.css";
+import Registeration from "./components/Registeration";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 function App() {
   return (
     <div className="App">
-     <h1>Dogs-club</h1>
+      {/* <h1>Dogs-club</h1> */}
+      <Registeration />
     </div>
   );
 }
